df9f3e12d2e57ca9864632e0050804a71b644fe4
Silvano Amaral <silvano.amaral@pmweb.com.br>
2018-01-16 16:15:06 -0200

    Adicionando Routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,9 +5,17 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { ModuleWithProviders } from "@angular/core/src/metadata/ng_module";
 
-const APP_ROUTES: Routes = [
-  { path: 'Contact', component: ContactComponent, data: { title: 'Heroes List' }  },
-  { path: '', component: HomeComponent, data: {
+const APP_ROUTES: Routes = [{ 
+    path: 'Contact', 
+    component: ContactComponent, 
+    data: { 
+      title: 'Heroes List' 
+    }  
+  },
+  { 
+    path: '', 
+    component: HomeComponent, 
+    data: {
       meta: {
         title: 'Home page',
         description: 'Description of the home page'
